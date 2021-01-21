f1fffdb7448e741699bf8bb929f39b90074a15ea
Aroma <songqin0000@163.com>
2021-01-21 13:41:33 +0800

    init

---
+++ lib/network/application.ex
@@ -0,0 +1,20 @@
+defmodule Network.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Network.Worker.start_link(arg)
+      # {Network.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Network.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
